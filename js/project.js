8ccd34e6aa0ca8a5b8ba28aebbeba5f5da20d814
AxelGuillou <axel.guillou@etu.unice.fr>
2023-02-16 20:32:09 +0100

    Modification

--- js/project.js
+++ js/project.js
@@ -34,8 +34,35 @@ function textproject2(){
         i.innerHTML = txt2;
     }
     
-    let btn2 = document.querySelector("div.project input[value]");
-    console.log(btn2);
-    let btn = document.getElementById("btn");
-    btn.innerHTML = "En savoir moins";
+    let btn3 = document.querySelector('input[id="btn2"]');
+    
+    if(btn3.value == "+"){
+        btn3.value = "En savoir moins";
+    }
+    else{
+        btn3.value = "+";
+    }
+}
+
+
+function textproject3(){
+    let txt = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget finibus ligula, convallis aliquam metus.Fusce vitae viverra sapien. Nam et dapibus nisl, vitae tempus sapien. Morbi lacus purus, pretium et eros ut, dictum venenatis arcu. Fusce ut blandit quam. Sed a rutrum odio, vel auctor ex. In hac habitasse platea dictumst.     Pellentesque sed felis justo. Proin auctor pharetra finibus. Pellentesque quam orci aliquet et diam id, imperdiet     hendrerit arcu. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus faucibus     odio nec suscipit faucibus. Ut tincidunt dui eros, at viverra diam rhoncus eu. Nam tortor metus, lobortis ac orci ac,     tincidunt mollis arcu.";
+    let txt2 = ""
+    let i = document.getElementById("3");
+    console.log(i.innerHTML);
+    if (i.innerHTML=="") {
+        i.innerHTML=txt;
+    }
+    else{
+        i.innerHTML = txt2;
+    }
+    
+    let btn3 = document.querySelector('input[id="btn3"]');
+    
+    if(btn3.value == "+"){
+        btn3.value = "En savoir moins";
+    }
+    else{
+        btn3.value = "+";
+    }
 }
